f7f93059d4db614239c1e547751842cfa8c8e5fe
elenavelte <elenavelte77@gmail.com>
2017-09-15 23:00:13 +0100

    create project skeleton

---
+++ readme.md
@@ -0,0 +1,2 @@
+**This is your readme file!**
+*add your documentation here...*
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quora',
+      version=version,
+      url='http://quora.org',
+      description='',
+      author='',
+      license='',
+      packages=[],
+      zip_safe=False,
+      install_requires=[])
