1787ead107d70713ef1db422a50d83238bb8e097
elenavelte <elena@optylon.com>
2017-09-18 21:01:11 +0100

    remove old code

--- bin/load_data.py
+++ bin/load_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import quora
 import os
 import warnings
+import time
 import numpy as np
 from quora.feature_engineering import calc_similarity
 from quora.feature_engineering import extract_features, add_some_columns
@@ -55,23 +56,23 @@ if __name__ == '__main__':
 
     # first time load and process data
     # load_data('train')
+
+    # concatenate all training questions into one dataframe to be used as corpus
     # store_all_train_questions()
 
-    import time
-    start_time = time.time()
+    start_time = time.time()  # count run time
 
+    # load dataframe with word and char counts... remove rows with less than 3 words and drop nans
+    # TODO: consider some further outlier analysis
     df = pd.read_pickle(os.path.join(quora.root, 'data', 'train_counts.pkl'))
-
-    # remove rows with less than 3 words
     df = df[(df.q1_n_words > 2) & (df.q2_n_words > 2)]
-
     df.dropna(inplace=True)
 
-    # run on complete dataset
-    dfs = df
-    # dfs = df.head(5000)
+    dfs = df  # run on complete data set
     print(dfs.columns.values)
 
+    # extract features from questions
+    # ------------------------------------------------------------------------------------------------------------------
     dfs = add_some_columns(dfs)
     features = dfs.apply(lambda row: extract_features(row[2], row[3], row[9], row[10], row[4]), axis=1)
 
@@ -81,9 +82,8 @@ if __name__ == '__main__':
         dfs['synsets1'], dfs['synsets2'], dfs['similarity'], dfs['word_share'], dfs['overlap'], dfs['diff'], \
             dfs['word_diff'], dfs['char_diff'], dfs['cosine_sim'], dfs['cosine_sim2'] = zip(*features)
 
-    print(dfs)
-
+    # pickle processed dataframe
     dfs.to_pickle(os.path.join(quora.root, 'data', 'processed_v6_all.pkl'))
 
-    elapsed_time = time.time() - start_time
+    elapsed_time = time.time() - start_time  # count run time
     print('Elapsed time: {}'.format(elapsed_time))
