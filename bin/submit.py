9218572efe00094f032f18146d5b88161752a8c0
elenavelte <elena@optylon.com>
2017-09-18 19:56:33 +0100

    fix submissions to be processed in chuncks

--- bin/submit.py
+++ bin/submit.py
@@ -1,8 +1,11 @@
 import os
 import quora
 import pandas as pd
-from quora.feature_engineering import extract_features
+from quora.feature_engineering import extract_features, add_some_columns
 import shelve
+from quora.aux_functions import get_cols_with_nans
+from sklearn.externals import joblib
+import numpy as np
 from sklearn.externals import joblib
 
 
@@ -10,41 +13,43 @@ from sklearn.externals import joblib
 file = os.path.join(quora.root, 'data', 'test.csv')
 df = pd.DataFrame.from_csv(file, index_col=None)
 
-print(df.shape)
-print(df.columns.values)
+# there are nans in the test questions! replace them with empty strings
+# get_cols_with_nans(df)
+df.fillna("", inplace=True)
 
-df = df[0:10]
-ids = df['test_id']
+# process test file in chunks:
+for chunk, df in df.groupby(np.arange(len(df))//100000):  # chunks of 300,000 would allow ~8 equal partitions
 
-# extract features from test data
-# test_id, question1, question2
-features = df.apply(lambda row: extract_features(row[1], row[2]), axis=1)
+    chunk += 1
+    print('Chunk #{}'.format(chunk))
+    print(df)
 
-dfs = pd.DataFrame()
+    # extract features from test data
+    # test_id, question1, question2
+    df = add_some_columns(df)
 
-dfs['synsets1'], dfs['synsets2'], dfs['similarity'], dfs['word_share'], dfs['overlap'], dfs['diff'], \
-            dfs['word_diff'], dfs['char_diff'] = zip(*features)
+    features = df.apply(lambda row: extract_features(row[1], row[2], row[3], row[4], log=False), axis=1)
 
-X = dfs[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff']]
+    dfs = pd.DataFrame()
 
-# # load from file
-# model = joblib.load('model.pkl')
-#
-# # apply trained model
-# res = pd.Series(model.predict(X))
+    dfs['synsets1'], dfs['synsets2'], dfs['similarity'], dfs['word_share'], dfs['overlap'], dfs['diff'], \
+                dfs['word_diff'], dfs['char_diff'], dfs['cosine_sim'], dfs['cosine_sim2'] = zip(*features)
 
-# # load from file
-rf = joblib.load('model0.pkl')
-rf_enc = joblib.load('model1.pkl')
-rf_lm = joblib.load('model2.pkl')
+    X = dfs[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim', 'cosine_sim2']]
 
-res = pd.Series(rf_lm.predict(rf_enc.transform(rf.apply(X))))
+    joblib.dump(df, os.path.join(quora.root, 'data', 'submission', 'test_X_chunk{}.pkl'.format(chunk)), compress=True)
 
+    # # load from file
+    rf = joblib.load('model0.pkl')
+    rf_enc = joblib.load('model1.pkl')
+    rf_lm = joblib.load('model2.pkl')
 
-# store result to csv
-# test_id, is_duplicated
-df_res = pd.concat([ids, res], axis=1, ignore_index=True)
-df_res.columns = ['test_id', 'is_duplicated']
-print(df_res)
+    res = pd.Series(rf_lm.predict(rf_enc.transform(rf.apply(X))))
 
-df_res.to_csv('submission_v2.csv', index=False)
+    # store result to csv
+    # test_id, is_duplicated -- concatenated by columns
+    df_res = pd.concat([d.reset_index(drop=True) for d in [df.test_id, res]], axis=1)
+    df_res.columns = ['test_id', 'is_duplicated']
+
+    f = os.path.join(quora.root, 'data', 'submission', '{}_submission_chunk{}.csv'.format(str(chunk).zfill(3), chunk))
+    df_res.to_csv(f, index=False)
