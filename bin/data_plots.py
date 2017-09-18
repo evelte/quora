a733922310abad4a43d487b8fb63cf6aed2c2af5
elenavelte <elena@optylon.com>
2017-09-18 22:08:10 +0100

    add distribution and violin plots

--- bin/data_plots.py
+++ bin/data_plots.py
@@ -6,6 +6,9 @@ import os
 import quora
 
 
+# get data and setup
+# ======================================================================================================================
+
 # load processed data
 df = pd.read_pickle(os.path.join(quora.root, 'data', 'processed_v6_all.pkl'))
 
@@ -15,7 +18,12 @@ df.dropna(inplace=True)
 # path to save plots
 path = os.path.join(quora.root, 'data', 'plots')
 
+
+# plot stuff!
+# ======================================================================================================================
+
 # 1. take a look at the word and char counts
+# ----------------------------------------------------------------------------------------------------------------------
 all_words = pd.concat([df.q1_n_words, df.q2_n_words], axis=0)
 all_chars = pd.concat([df.q1_n_chars, df.q2_n_chars], axis=0)
 
@@ -27,21 +35,30 @@ ax1.set_xlabel('Word count')
 ax2.set_xlabel('Character count')
 fig.savefig(os.path.join(path, "word_and_char_count.png"))
 
-# cols = ['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim']
-#
-# df = pd.read_pickle(os.path.join(quora.root, 'data', 'processed_v3_5000.pkl'))
-#
-# # conditional coloring based one one variable (should be categorical)
-# sns.pairplot(df[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim', 'is_duplicate']],
-#              hue='is_duplicate')
-#
-# sns.plt.show()
-#
-# for col in cols:
-#     sns.plt.figure()
-#     # sns.distplot(df[df['is_duplicate'] == 1.0][col], color='red')
-#     # sns.distplot(df[df['is_duplicate'] == 0.0][col], color='green')
-#
-#     sns.violinplot(x='is_duplicate', y=col, data=df)
-#
-# sns.plt.show()
+
+# 2. pair plot of all extracted features
+# ----------------------------------------------------------------------------------------------------------------------
+# conditional coloring based on one variable - is_duplicate
+fig, ax = plt.subplots()
+sns.pairplot(df[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim',
+                 'is_duplicate']], hue='is_duplicate')
+# fig.savefig(os.path.join(path, "pair_plot.png"))
+
+
+# 3. violin plot for extracted features: compare is_duplicate True/False
+# ----------------------------------------------------------------------------------------------------------------------
+cols = ['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim']
+
+for col in cols:
+    fig, ax = plt.subplots()
+    sns.violinplot(x='is_duplicate', y=col, data=df)
+    fig.savefig(os.path.join(path, "violin_{}.png".format(col)))
+
+
+# 3.b show distribution instead of violin
+# ----------------------------------------------------------------------------------------------------------------------
+for col in cols:
+    fig, ax = plt.subplots()
+    sns.distplot(df[df['is_duplicate'] == 1.0][col], color='red')
+    sns.distplot(df[df['is_duplicate'] == 0.0][col], color='green')
+    fig.savefig(os.path.join(path, "distribution_{}.png".format(col)))
