8a1348ab59f7fe08665755538f69fe4d6bfcc7ee
elenavelte <elena@optylon.com>
2017-09-20 19:30:01 +0100

    refactor

--- bin/submission.py
+++ bin/submission.py
@@ -7,7 +7,7 @@ from quora.feature_engineering import extract_features, add_some_columns
 from quora.aux_functions import get_cols_with_nans, concatenate_csvs
 
 
-# load test data
+# 1. load test data
 # ======================================================================================================================
 file = os.path.join(quora.root, 'data', 'test.csv')
 df = pd.DataFrame.from_csv(file, index_col=None)
@@ -16,33 +16,63 @@ df = pd.DataFrame.from_csv(file, index_col=None)
 # get_cols_with_nans(df)
 df.fillna("", inplace=True)
 
-# process test file in chunks:
+# 2. process/load test file in chunks:
 # ======================================================================================================================
-for chunk, df in df.groupby(np.arange(len(df))//100000):  # chunks of 300,000 would allow ~8 equal partitions
+for chunk, df in df.groupby(np.arange(len(df)) // 100000):  # chunks of 300,000 would allow ~8 equal partitions
 
     chunk += 1
-    print('Processing chunk #{}'.format(chunk))
 
-    # 1. extract features from test data | test_id, question1, question2
+    option = 'load'  # CHANGE OPTION HERE! options: load or process
+    load_v = 'v3'
+    store_v = 'v3'
+    model_v = 'v3'
+
+    if option == 'process':  # do not change this
+
+        print('Processing chunk #{}'.format(chunk))
+
+        # 1. extract features from test data | test_id, question1, question2
+        # --------------------------------------------------------------------------------------------------------------
+        df = add_some_columns(df)
+        features = df.apply(lambda row: extract_features(row[1], row[2], row[3], row[4], log=False), axis=1)
+
+        dfs = pd.DataFrame()
+        dfs['synsets1'], dfs['synsets2'], dfs['similarity'], dfs['word_share'], dfs['overlap'], dfs['diff'], \
+        dfs['word_diff'], dfs['char_diff'], dfs['cosine_sim'], dfs['cosine_sim2'], dfs['key_share'], dfs['topic_sim'] \
+            = zip(*features)
+
+        # 3. store pre-processed dataframe with test data chunk
+        # --------------------------------------------------------------------------------------------------------------
+        joblib.dump(dfs, os.path.join(quora.root, 'data', 'submission', store_v,
+                    'test_X_chunk{}.pkl'.format(chunk)),
+                    compress=True)
+
+        ids = df.test_id
+
+    elif option == 'load':  # do not change this
+
+        print('Loading chunk #{}'.format(chunk))
+
+        # specify version here...
+        dfs = joblib.load(os.path.join(quora.root, 'data', 'submission', load_v, 'test_X_chunk{}.pkl'.format(chunk)))
+
+        interval = ((chunk - 1) * 100000)
+        start = 0 + interval
+        end = start + 100000
+        ids = pd.Series(range(start, end))
+
+        # the last chunk will have more ids than data points
+        if len(dfs) < len(ids):
+            ids = ids[0:len(dfs)]
+
+    # 3. define dataframe to apply models on
     # ------------------------------------------------------------------------------------------------------------------
-    df = add_some_columns(df)
-    features = df.apply(lambda row: extract_features(row[1], row[2], row[3], row[4], log=False), axis=1)
-
-    dfs = pd.DataFrame()
-    dfs['synsets1'], dfs['synsets2'], dfs['similarity'], dfs['word_share'], dfs['overlap'], dfs['diff'], \
-                dfs['word_diff'], dfs['char_diff'], dfs['cosine_sim'], dfs['cosine_sim2'] = zip(*features)
-
-    # 2. define dataframe to apply models on
-    # ------------------------------------------------------------------------------------------------------------------
-    X = dfs[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim', 'cosine_sim2']]
-
-    # 3. store pre-processed dataframe with test data chunk
-    # ------------------------------------------------------------------------------------------------------------------
-    joblib.dump(dfs, os.path.join(quora.root, 'data', 'submission', 'test_X_chunk{}.pkl'.format(chunk)), compress=True)
+    X = dfs[['similarity', 'word_share', 'overlap', 'diff', 'word_diff', 'char_diff', 'cosine_sim', 'cosine_sim2',
+             'key_share', 'topic_sim']]
 
     # 4. load trained models from file
     # ------------------------------------------------------------------------------------------------------------------
-    path = os.path.join(quora.root, 'data', 'models')
+    path = os.path.join(quora.root, 'data', 'models', model_v)
 
     rf = joblib.load(os.path.join(path, 'model0.pkl'))
     rf_enc = joblib.load(os.path.join(path, 'model1.pkl'))
@@ -50,19 +80,19 @@ for chunk, df in df.groupby(np.arange(len(df))//100000):  # chunks of 300,000 wo
 
     # 5. apply models to test data chunk
     # ------------------------------------------------------------------------------------------------------------------
-    res = rf_lm.predict_proba(rf_enc.transform(rf.apply(X_test)))[:, 1]
+    res = rf_lm.predict_proba(rf_enc.transform(rf.apply(X)))[:, 1]
 
     # 6. construct final dataset | test_id, is_duplicated -- concatenated by columns
     # ------------------------------------------------------------------------------------------------------------------
-    df_res = pd.concat([d.reset_index(drop=True) for d in [df.test_id, pd.Series(res)]], axis=1)
+    df_res = pd.concat([d.reset_index(drop=True) for d in [ids, pd.Series(res)]], axis=1)
     df_res.columns = ['test_id', 'is_duplicated']
 
     # 7. store result from each chunk to csv
     # ------------------------------------------------------------------------------------------------------------------
-    f = os.path.join(quora.root, 'data', 'submission', '{}_submission_chunk{}.csv'.format(str(chunk).zfill(3), chunk))
-    df_res.to_csv(f, index=False)
-
+    file = '{}_submission_chunk{}.csv'.format(str(chunk).zfill(3), chunk)
+    path = os.path.join(quora.root, 'data', 'submission', store_v, file)
+    df_res.to_csv(path, index=False)
 
 # concatenate results
 # ======================================================================================================================
-concatenate_csvs()
+concatenate_csvs(store_v)
